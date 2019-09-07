7cd359f22cad293c200af5b6267ee2f2da2c0d94
destiny_123 <akrati.chaturvedi_cs16@gla.ac.in>
2019-09-07 13:54:28 +0530

    updated views.py (07-Aug-2019)

--- VendorFinder/services/views.py
+++ VendorFinder/services/views.py
@@ -6,3 +6,51 @@ def index(request):
     context = {}
     template_name = 'services/index.html'
     return render(request, template_name=template_name, context=context)
+
+
+def cutomer_profile(request):
+    context = {}
+    template_name = 'services/customer_profile.html'
+    return render(request, template_name=template_name, context=context)
+
+
+def vendor_profile(request):
+    context = {}
+    template_name = 'services/vendor_profile.html'
+    return render(request, template_name=template_name, context=context)
+
+
+def vendor_customer_request_page(request):
+    context = {}
+    template_name = 'services/request_page.html'
+    return render(request, template_name=template_name, context=context)
+
+
+def vendor_filter_page(request):
+    context = {}
+    template_name = 'services/vendor_filter.html'
+    return render(request, template_name=template_name, context=context)
+
+
+def vendor_profile(request):
+    context = {}
+    template_name = 'services/vendor_profile.html'
+    return render(request, template_name=template_name, context=context)
+
+
+def vendor_profile(request):
+    context = {}
+    template_name = 'services/vendor_profile.html'
+    return render(request, template_name=template_name, context=context)
+
+
+def vendor_profile(request):
+    context = {}
+    template_name = 'services/vendor_profile.html'
+    return render(request, template_name=template_name, context=context)
+
+
+
+
+
+
