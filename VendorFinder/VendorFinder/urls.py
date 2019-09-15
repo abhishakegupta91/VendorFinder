20bcd4e79995b8780d73e6765b63f5fff913da8e
destiny_123 <akrati.chaturvedi_cs16@gla.ac.in>
2019-09-15 16:09:53 +0530

    updated vendorFinder/urls.py

--- VendorFinder/VendorFinder/urls.py
+++ VendorFinder/VendorFinder/urls.py
@@ -22,7 +22,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('services/', include(services_urls)),
+  #  path('services/', include(services_urls)),
+     path('api/vi/', include(services_urls)),
 ]
 
 if settings.DEBUG:
