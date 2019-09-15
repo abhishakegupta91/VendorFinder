e225d9f790a9a2c229bf011a71c6d367b4725890
destiny_123 <akrati.chaturvedi_cs16@gla.ac.in>
2019-09-15 16:07:47 +0530

    updated urls.py

--- VendorFinder/services/urls.py
+++ VendorFinder/services/urls.py
@@ -3,4 +3,9 @@ from services import views as ser_views
 
 urlpatterns = [
     path('', ser_views.index),
+    path('customer-profile', ser_views.cutomer_profile),
+    path('vendor-profile', ser_views.vendor_profile),
+    path('request-page', ser_views.vendor_customer_request_page),
+    path('vendor-filter', ser_views.vendor_filter_page),
+
 ]
