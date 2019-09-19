aff5b3524575af72f9c0dc9e9b8e68b55d7e9e2d
akshaykatiha <akshay.katiha_cs16@gla.ac.in>
2019-09-19 22:59:00 +0530

    Updating forms.py to allow signup

--- VendorFinder/services/forms.py
+++ VendorFinder/services/forms.py
@@ -1,17 +1,15 @@
 from django import forms
-from django.contrib.auth import get_user_model
-User = get_user_model()
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
-class UserCreationForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm):
         model = User
         fields = ('username', 'email')
 
-class UserChangeForm(UserChangeForm):
+class CustomUserChangeForm(UserChangeForm):
 
-    class Meta(UserChangeForm):
+    class Meta:
         model = User
         fields = ('username', 'email')
