ee11c19de393fce74ee99d1cd8f79ca366dc4221
akshaykatiha <akshay.katiha_cs16@gla.ac.in>
2019-09-19 22:57:13 +0530

    Updating Table name and column names in models.py

--- VendorFinder/services/models.py
+++ VendorFinder/services/models.py
@@ -1,7 +1,11 @@
 # Import
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
+from django.core.validators import (
+    RegexValidator,
+    MaxValueValidator,
+    MinValueValidator
+)
 
 
 # CHOICE_FIELDS
@@ -12,22 +16,10 @@ SEX_CHOICE = [
 ]
 
 # Models Here
-class Status(models.Model):
-    is_active = models.BooleanField(default=False)
-    status_name = models.CharField(max_length=124)
-    description = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-            return self.status_name
-    class Meta:
-        db_table = 'status'
-
-
 class User(AbstractUser):
     is_customer = models.BooleanField(default=False)
     is_vendor = models.BooleanField(default=False)
-    phone = models.CharField(validators=[RegexValidator(regex=r'^[9876]\d{9}$', message="Please Enter Correct Phone Number", code='nomatch'
+    phone = models.CharField(validators=[RegexValidator(regex=r'^[9876]\d{9}$', message="Enter 10 digit mobile number without any prefix(+91 or 0)", code='nomatch'
     )],
     max_length=10)
     sex = models.CharField(max_length=1, choices=SEX_CHOICE, blank=True)
@@ -35,15 +27,35 @@ class User(AbstractUser):
     address = models.CharField(max_length=124,blank=True)
     city = models.CharField(max_length=124,blank=True)
     state = models.CharField(max_length=124,blank=True)
-    last_modified = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    #row_insert_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_insert_date = models.DateTimeField(auto_now_add=True)
+    #row_update_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_update_date = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
-        return self.username
+        return self.first_name
     class Meta:
         db_table = 'user'
+        verbose_name_plural = 'user'
 
 
+# class Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+#     customer_served = models.PositiveSmallIntegerField()
+#     service_taken = models.PositiveSmallIntegerField()
+#     rating = models.PositiveSmallIntegerField()
+#     #row_insert_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+#     #row_insert_date = models.DateTimeField(auto_now_add=True)
+#     #row_update_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+#     #row_update_date = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#             return self.user.username
+#     class Meta:
+#         db_table = 'profile'
+
+    
 class Vendor(models.Model):
     vendor = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     customer_served = models.PositiveSmallIntegerField()
@@ -51,36 +63,32 @@ class Vendor(models.Model):
     #number_of_services = models.PositiveSmallIntegerField() #Used to track total number of services served by a vendor
     #active_since = models.DateField()
 
-    def __str__(self):
-       return self.user.username
-
+    # def __str__(self):
+    #    return self.user.username
     class Meta:
         db_table = 'vendor'
-
+        verbose_name_plural = 'vendor'
 
 class Customer(models.Model):
     customer = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     service_taken = models.PositiveSmallIntegerField()
-    #active_since = models.DateField()
-    
-    def __str__(self):
-       return self.user.username
-
+    #active_since = models.DateField()    
+    # def __str__(self):
+    #    return self.user.username
     class Meta:
         db_table = 'customer'
-
+        verbose_name_plural = 'customer'
 
 class Admin(models.Model):
     admin = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     sex = models.CharField(max_length=1, choices=SEX_CHOICE, blank=True)
     #qualification = model.TextField()
     #member_since = model.DateTime(auto_now=True)
-
-    def __str__(self):
-       return self.user.username
-
+    # def __str__(self):
+    #    return self.user.username
     class Meta:
         db_table = 'admin'
+        verbose_name_plural = 'admin'
 
 
 class Otp(models.Model):
@@ -88,43 +96,73 @@ class Otp(models.Model):
     generation_time = models.DateTimeField(auto_now=True)
     #expiry_time = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-       return self.user.username
+    # def __str__(self):
+    #    return self.user.username
 
     class Meta:
         db_table = 'otp'
+        verbose_name_plural = 'otp'
+
+
+class Status(models.Model):
+    is_active = models.BooleanField(default=False)
+    status_name = models.CharField(max_length=124)
+    description = models.TextField()
+    #row_insert_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_insert_date = models.DateTimeField(auto_now_add=True)
+    # row_update_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    # row_update_date = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+            return self.status_name
+    class Meta:
+        #abstract = True
+        #If in future, we are not interested to create table for Status
+        #we can set abstract = True
+        db_table = 'status'
+        verbose_name_plural = 'status'
 
 
 class Service(models.Model):
     is_active = models.BooleanField(default=False)
     service_name = models.CharField(max_length=124)
     service_charge = models.DecimalField(max_digits=10, decimal_places=2)
-    #admin = models.ForeignKey(Admin, models.SET_NULL)
+    
     #To keep track of admin, who has added the service
-    last_modified = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    #row_insert_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_insert_date = models.DateTimeField(auto_now_add=True)
+    #row_update_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_update_date = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
        return self.service_name
 
     class Meta:
         db_table = 'service'
+        verbose_name_plural = 'service'
 
 
 class SubService(models.Model):
     is_active = models.BooleanField(default=False)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     sub_service_name = models.CharField(max_length=124)
-    #admin = models.ForeignKey(Admin, models.SET_NULL)
+    
     #To keep track of admin, who has added the service
-    last_modified = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    #row_insert_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_insert_date = models.DateTimeField(auto_now_add=True)
+
+    #To keep track of admin, who has added the service
+    #row_update_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_update_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.sub_service_name
 
     class Meta:
         db_table = 'sub_service'
+        verbose_name_plural = 'sub service'
 
 
 class VendorService(models.Model):
@@ -133,17 +171,20 @@ class VendorService(models.Model):
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
     sub_service = models.ForeignKey(SubService, on_delete=models.CASCADE)
     sub_service_charge = models.DecimalField(max_digits=10, decimal_places=2)
-    last_modified = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    #row_insert_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_insert_date = models.DateTimeField(auto_now_add=True)
+    #row_update_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_update_date = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-       return self.sub_service.sub_service_name
-    # What should be returned : Vendor's name, service name or sub_service name
+
+    # def __str__(self):
+    #    return self.sub_service.sub_service_name
 
     class Meta:
         db_table = 'vendor_service'
+        verbose_name_plural = 'vendor service'
 
-class VendorCustomer(models.Model):
+class Invoice(models.Model):
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
@@ -151,28 +192,25 @@ class VendorCustomer(models.Model):
     status = models.ForeignKey(Status,on_delete=models.SET_NULL, null=True)
     total_charge = models.DecimalField(max_digits=10, decimal_places=2)
     description = models.TextField()
-    last_modified = models.DateTimeField(auto_now=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
-
-    #Name of the vendor must be set as default, because it is possible that when vendor is deleted,
-    #customer is not deleted, and the record must exist. Same thing applies in case of customer
-    # i.e. Either on delete, null value is set as default value (models.SET_NULL) or the name of 
-    #vendor or customer should be set from database.
+    #row_insert_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_insert_date = models.DateTimeField(auto_now_add=True)
+    #row_update_by = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    row_update_date = models.DateTimeField(auto_now=True)
 
     #def __str__(self):
     #    return self.service.service_name
-    # What should be returned : Vendor's name, service name or sub_service name
+    # What should be returned?
 
     class Meta:
-        db_table = 'vendor_customer'
+        db_table = 'invoice'
+        verbose_name_plural = 'invoice'
 
 
 # class Invoice(models.Model):
 #     pass
 
-#     #def __str__(self):
-#     #    return self.service.service_name
-#     # What should be returned : Vendor's name, service name or sub_service name
+#     def __str__(self):
+#         pass
 
 #     class Meta:
 #         db_table = 'otp'
