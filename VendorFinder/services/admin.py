5fbdf3a0654e1fd3517f240adebea75fd14ec083
akshaykatiha <akshay.katiha_cs16@gla.ac.in>
2019-09-19 23:09:33 +0530

    Updating admin.py to resolve signup issue

--- VendorFinder/services/admin.py
+++ VendorFinder/services/admin.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-User = get_user_model()
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import UserCreationForm, UserChangeForm
-from .models import User, Vendor, Customer, Admin, Service, SubService, Status, VendorService, VendorCustomer
-class UserAdmin(UserAdmin):
-    add_form = UserCreationForm
-    form = UserChangeForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import (
+    User,
+    Vendor,
+    Customer,
+    Admin,
+    Service,
+    SubService,
+    Status,
+    VendorService,
+    Invoice
+)
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = User
     list_display = ['email', 'username',]
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Vendor)
 admin.site.register(Customer)
 admin.site.register(Admin)
@@ -19,5 +29,5 @@ admin.site.register(Service)
 admin.site.register(SubService)
 admin.site.register(Status)
 admin.site.register(VendorService)
-admin.site.register(VendorCustomer)
+admin.site.register(Invoice)
 #admin.site.register()
